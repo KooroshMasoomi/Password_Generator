5ef1b863c7f14b12e7edd4ef5983b881272a179a
Koorosh Masoomi <koorosh2006masoomi@gmail.com>
2023-03-16 16:03:32 +0330

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,39 @@
+from string import ascii_uppercase, ascii_lowercase, digits, punctuation
+from random import choices
+from os import system
+import sys
+
+UPPERCASE = True
+LOWERCASE = False
+NUMBERS = True
+SYMBOLS = False
+
+if len(sys.argv) == 1:
+    LENGTH = 10
+elif len(sys.argv[1:]) == 1:
+    if sys.argv[1].isdigit():
+        LENGTH = int(sys.argv[1:][0])
+    else:
+        print("The input is not a number!")
+elif len(sys.argv[1:]) > 1:
+    print("The entry is more than one item!")
+
+settings = {
+    "UPPERCASE": ascii_uppercase if UPPERCASE else False,
+    "LOWERCASE": ascii_lowercase if LOWERCASE else False,
+    "NUMBERS": digits if NUMBERS else False,
+    "SYMBOLS": punctuation if SYMBOLS else False,
+}
+
+letters = ""
+
+if list(settings.values()).count(False) == 4:
+    print("All settings options are disabled, cannot generate password!")
+    system("pause")
+    exit(1)
+else:
+    for option in settings.keys():
+        if settings[option]:
+            letters += settings[option]
+
+print(*choices(list(letters), k=LENGTH), sep="")
---
+++ README.md
@@ -0,0 +1,26 @@
+# Password Generator
+
+## This program creates a password for you with the settings you apply.
+
+<hr>
+
+### To set the default password length, you can set the LENGTH variable, which is set to 10 characters by default:
+
+<br>
+
+```python
+LENGTH = 10
+```
+
+<hr>
+
+### To use or not use options such as the presence of lowercase letters in the password, uppercase letters, numbers and symbols, you can confirm or reject each of them, which are set by default as follows:
+
+<br>
+
+```python
+UPPERCASE = True
+LOWERCASE = False
+NUMBERS = True
+SYMBOLS = False
+```
